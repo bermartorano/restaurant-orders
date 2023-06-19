8fb8726882b8449ebd82ca9155dce9b74ea869e0
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-19 20:10:30 -0300

    convertion from csv to dict function implemented

--- utils/dish_dict_formater.py
+++ utils/dish_dict_formater.py
@@ -1,22 +1,38 @@
 from utils.csv_read import csv_data
 
-def format_csv_to_dict(data: list[list[str]]) -> list[dict]:
-    final_list = []
-    for ingredient_info in data:
-        final_dict = {}
-        final_dict['name'] = ingredient_info[0]
-        final_dict['price'] = float(ingredient_info[1])
-        try:
-            final_dict['ingredients'] = [{
-                'ingredient_name': ingredient_info[2],
-                'ingredient_qnt': int(ingredient_info[3])
-            }]
-        except KeyError:
-            final_dict['ingredients'].append({
-                'ingredient_name': ingredient_info[2],
-                'ingredient_qnt': int(ingredient_info[3])
-            })
-        final_list.append(final_dict)
-    return final_list
+def format_csv_to_dict(data: list[list[str]]) -> dict:
+    # final_dict = {}
+    # for ingredient_info in data:
+    #     final_dict[ingredient_info[0]] = {
+    #         'nome': ingredient_info[0],
+    #         'price': float(ingredient_info[1]),
+    #     }
+    #     ingredient_to_add = {
+    #         'ingredient_name': ingredient_info[2],
+    #         'ingredient_qnt': ingredient_info[3]
+    #     }
+    #     try:
+    #         final_dict[ingredient_info[0]]['ingredients'].append(ingredient_to_add)
+    #     except KeyError:
+    #         final_dict[ingredient_info[0]]['ingredients'] = [ingredient_to_add]
 
-print(format_csv_to_dict(csv_data('/home/bernardo/trybe/projetos/ciencia-da-computacao/sd-026-b-restaurant-orders/data/menu_base_data.csv')))
+    final_dict = {}
+    for ingredient_info in data:
+        ingredient_to_add = {
+                'ingredient_name': ingredient_info[2],
+                'ingredient_qnt': ingredient_info[3]
+            }
+        try:
+            final_dict[ingredient_info[0]]['name'] = ingredient_info[0]
+            final_dict[ingredient_info[0]]['price'] = float(ingredient_info[1])
+            final_dict[ingredient_info[0]]['ingredients'].append(ingredient_to_add)
+        except KeyError:
+            final_dict[ingredient_info[0]] = {
+                'name': ingredient_info[0],
+                'price': float(ingredient_info[1]),
+            }
+            final_dict[ingredient_info[0]]['ingredients'] = [ingredient_to_add]
+    return final_dict
+
+
+print(format_csv_to_dict(csv_data('/home/bernardo/trybe/projetos/ciencia-da-computacao/sd-026-b-restaurant-orders/data/menu_base_data.csv'))['lasanha presunto'])
