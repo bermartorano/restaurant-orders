6a783afae8928e190f2cbce7d494886610c2c290
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-20 19:29:03 -0300

    req04

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,30 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+from pytest import raises
 
 
 # Req 2
 def test_dish():
-    pass
+    info_1 = ['macarrão', 20.00]
+    info_2 = ['sushi', 100.00]
+    dish_1 = Dish(info_1[0], info_1[1])
+    dish_2 = Dish(info_1[0], info_1[1])
+    dish_3 = Dish(info_2[0], info_2[1])
+
+    ingredient = Ingredient('farinha')
+    dish_1.add_ingredient_dependency(ingredient, 1.5)
+
+    assert dish_1.name == info_1[0]
+    assert hash(dish_1) == hash(dish_2)
+    assert hash(dish_1) != hash(dish_3)
+    assert dish_1 == dish_2
+    assert dish_1 != dish_3
+    assert dish_1.__repr__() == "Dish('macarrão', R$20.00)"
+
+    with raises(TypeError, match='Dish price must be float.'):
+        Dish('nome', 'preco_errado')
+    with raises(ValueError, match='Dish price must be greater then zero.'):
+        Dish('nome', -1.00)
+
+    assert dish_1.get_restrictions() == {Restriction.GLUTEN}
+    assert dish_1.get_ingredients() == {ingredient}
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,14 +3,14 @@ from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E
 
 # Req 1
 def test_ingredient():
-    ingred_1 = Ingredient('lasanha')
-    ingred_2 = Ingredient('lasanha')
+    ingred_1 = Ingredient('sal')
+    ingred_2 = Ingredient('sal')
     ingred_3 = Ingredient('farinha')
 
     assert hash(ingred_1) == hash(ingred_2)
     assert hash(ingred_1) != hash(ingred_3)
     assert ingred_1 == ingred_2
     assert ingred_1 != ingred_3
-    assert ingred_1.__repr__() == "Ingredient('lasanha')"
-    assert ingred_1.name == 'lasanha'
+    assert ingred_1.__repr__() == "Ingredient('sal')"
+    assert ingred_1.name == 'sal'
     assert ingred_3.restrictions == {Restriction.GLUTEN}
